b7364adcd48b042db412961ae2a0d1fce552c989
Alireza Savand <alireza.savand@gmail.com>
2013-01-22 03:45:33 +0400

    let see what we have here
    What args user asked for

--- negar_cli/cli.py
+++ negar_cli/cli.py
@@ -31,3 +31,30 @@ def main():
         print(err)
         help_message()
         sys.exit(1)
+
+    for opt, arg in opts:
+        if opt in ['-h', '--help']:
+            help_message()
+            sys.exit(0)
+        elif opt in ['-V', '--Version']:
+            #TODO: get real version
+            print("You are using Negar version: __version__")
+        elif opt in ('--fix-non-persian-chars'):
+            args.append('fix-non-persian-chars')
+        elif opt in ('-f', '--file'): file_name = arg
+        elif opt in ('-o', '--output'): output_file = arg
+        elif opt in '--fix-dashes':  args.append('fix-dashes')
+        elif opt in '--fix-three-dots':  args.append('fix-three-dots')
+        elif opt in '--fix-english-quotes': args.append('fix-english-quotes')
+        elif opt in '--fix-hamzeh': args.append('fix-hamzeh')
+        elif opt in '--hamzeh-with-yeh': args.append('hamzeh-with-yeh')
+        elif opt in '--fix-spacing-bq': args.append('fix-spacing-bq')
+        elif opt in '--fix-arabic-num': args.append('fix-arabic-num')
+        elif opt in '--fix-english-num': args.append('fix-english-num')
+        elif opt in '--fix-p-spacing': args.append('fix-p-spacing')
+        elif opt in '--fix-p-separate': args.append('fix-p-separate')
+        elif opt in '--fix-s-separate': args.append('fix-s-separate')
+        elif opt in '--aggresive': args.append('aggresive')
+        elif opt in '--cleanup-kashidas': args.append('cleanup-kashidas')
+        elif opt in '--cleanup-ex-marks': args.append('cleanup-ex-marks')
+        elif opt in '--cleanup-spacing': args.append('cleanup-spacing')
