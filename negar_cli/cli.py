ca008e9ff9bc006f145c3191404fc99d2e58f3b5
Alireza Savand <alireza.savand@gmail.com>
2013-01-22 03:44:53 +0400

    Check if user typed command without any argument,
    show him how to use negar!

--- negar_cli/cli.py
+++ negar_cli/cli.py
@@ -10,3 +10,24 @@ def main():
     output_file = "Negar_Output"
     input_file = ''
     file_name = ''
+    # Check if user typed command without any argument,
+    # show him how to use negar!
+    if len(sys.argv) < 2:
+        help_message()
+        exit(0)
+    try:
+        opts, args = getopt.getopt(
+            sys.argv[1:],
+            "hVgf:o:",
+            ["help","file=","output=", "Version", "gui", "fix-dashes",
+             "fix-three-dots", "fix-english-quotes", "fix-hamzeh",
+             "hamzeh-with-yeh", "fix-spacing-bq", "fix-arabic-num",
+             "fix-english-num", "fix-non-persian-chars", "fix-p-spacing",
+             "fix-p-separate", "fix-s-spacing", "fix-s-separate", "aggresive",
+             "cleanup-kashidas", "cleanup-ex-marks", "cleanup-spacing",
+             "add-untouch-list="]
+        )
+    except getopt.GetoptError, err:
+        print(err)
+        help_message()
+        sys.exit(1)
