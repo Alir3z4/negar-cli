3bc73c66055f7f7fecc47cee759a3892e6101b85
Alireza Savand <alireza.savand@gmail.com>
2013-01-22 03:44:31 +0400

    Adde main()
    First initial some local variable

--- negar_cli/cli.py
+++ negar_cli/cli.py
@@ -1,2 +1,12 @@
 import sys
 import getopt
+from negar_cli.helpers import help_message
+
+def main():
+    """
+    main()
+    ======
+    """
+    output_file = "Negar_Output"
+    input_file = ''
+    file_name = ''
